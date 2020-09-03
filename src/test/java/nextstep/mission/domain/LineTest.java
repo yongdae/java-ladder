866cb34b6b2b58f5ec038725040b9065b3db71e5
yongdae <7942yongdae@gmail.com>
2020-09-03 20:26:52 +0900

    refactor: https://github.com/next-step/java-ladder/pull/593#discussion_r482679363 수정

--- src/test/java/nextstep/mission/domain/LineTest.java
+++ src/test/java/nextstep/mission/domain/LineTest.java
@@ -2,17 +2,21 @@ package nextstep.mission.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LineTest {
 
     @Test
     public void init() {
-        int sizeOfPerson = 5;
-        System.out.println(Line.init(sizeOfPerson));
+        int size = 5;
+        assertThat(Line.init(size).getPoints().size()).isEqualTo(size);
     }
 
     @Test
     public void move() {
-        Line line = Line.init(2);
-        System.out.println("ladder result : " + line.move(0));
+        assertThat(new Line(Arrays.asList(Point.first(false))).move(0)).isEqualTo(0);
+        assertThat(new Line(Arrays.asList(Point.first(true))).move(0)).isEqualTo(1);
     }
 }
